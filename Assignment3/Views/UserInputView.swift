a2a5ad60eceab74a5726f3b875ea62e92b2b187b
Dan Gunter <dvg0004@auburn.edu>
2023-03-23 16:33:25 -0600

    Uploading for review by Abhi

--- Assignment3/Views/UserInputView.swift
+++ Assignment3/Views/UserInputView.swift
@@ -13,40 +13,40 @@ struct UserInputView: View {
     // let orgID = "131" //131 is FS, 139 is BLM
     // @ObservedObject var facilityvm = RecreationViewModel()
      
-    @State private(set) var userValue=""
+    @State private(set) var userValue="CO"
+  
     @State private var showDetails = false
-    enum States: String, CaseIterable, Identifiable {
-        case CO, WY, UT, NM
-        var id: Self {self}
-    }
+    
+    var States = ["CO", "WY", "UT", "NM"]
+    
+//    enum Orgs: String, CaseIterable, Identifiable {
+//        case forest_service
+//        case blm //131 is forest service, 139 is BLM
+//        var id: Self {self}
+//    }
+    
     var body: some View {
         VStack {
             NavigationView {
                 List {
-                    //Text("Public Lands Facility Finder")
-                    //                 HStack {
-                    //                     Text("Enter State Abbreviation:")
-                    //                     TextField("", text: $userValue)
-                    //                         .textFieldStyle(.roundedBorder)
-                    //                         .keyboardType(.alphabet)
-                    //                 }
-                    Picker("State", selection: $userValue) {
-                        ForEach(States.allCases) {
-                            state in Text(state.rawValue)
+                    Picker("Select a state: ", selection: $userValue) {
+                        ForEach(States, id: \.self) {
+                            Text($0)
                         }
                     }
-                    Button("Find Facilities") {
-                        showDetails.toggle()
+                   Button("Find Facilities") {
+                       showDetails.toggle()
                     }
-                    
-                    
                 }
+                .frame(minWidth: 0, minHeight: 0, idealHeight: 100, maxHeight: 150)
                 .navigationTitle("Public Lands Facility Finder")
             }
+     
             if showDetails {
+                //showDetails.toggle()
                 FacilityNavigatorView(state: userValue)
-                // showDetails.toggle()
             }
+                            
         }
          
      }
